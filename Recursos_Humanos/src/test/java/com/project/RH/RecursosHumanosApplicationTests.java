91e2a2162ff84ca5034d16872ea9c6cbd937b688
Camila Melo <dantas.mill.melo@gmail.com>
2022-01-30 09:44:44 -0300

    .

---
+++ Recursos_Humanos/src/test/java/com/project/RH/RecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
